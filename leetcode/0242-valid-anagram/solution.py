00adc1bd62fd4bca7fd2980f50f5cbe8e366f11b
PurpleSwtr <sergeenko.mihail@yandex.ru>
2026-09-18 20:40:26 +0000

    [LeetCode Sync] Runtime - 15 ms (43.22%), Memory - 20.4 MB (10.04%)

--- leetcode/0242-valid-anagram/solution.py
+++ leetcode/0242-valid-anagram/solution.py
@@ -1,20 +1,5 @@
 class Solution:
-    def isAnagram(self, first: str, second: str) -> bool:
-        array = [0] * 26
-
-        for letter in first:
-            position = (ord(letter) - 97)
-            array[position] = array[position] + 1
-        
-        for letter in second:
-            position = (ord(letter) - 97)
-            array[position] = array[position] - 1
-
-        return all(x == 0 for x in array)
-
-
-
-
-
-
-        
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        return sorted(s) == sorted(t)
